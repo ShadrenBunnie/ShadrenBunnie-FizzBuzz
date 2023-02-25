be86438a8240d2e91ff9fdf11921d8d1d9a35237
Brande H <brandelinh@gmail.com>
2023-02-25 03:21:34 +0000

    FizzBuzzIfElses

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,30 @@ public class Lab {
      * @return if a number is divisible by 3, return 'Fizz'. if a number is divisible by 5, return 'Buzz'. If
      * divisible by both, return 'FizzBuzz'. If none of these are true, return a blank string "".
      */
+
+     /** The goal of this challenge is to determine which string line to be printed based upon
+      * the divisible number
+      * if n %3 = 0
+      *     return "Fizz"
+      * if n %5 = 0
+      *      return "Buzz"
+      * if n %3 = 0 & n %5 = 0
+      *    return "FizzBuzz"
+      *else
+      *     return ""
+      * 
+      * @param n
+      * @return
+      */
     public String FizzBuzzGen(int n){
-        return "";
+        if (n%3==0 && n%5 ==0){
+            return "FizzBuzz";
+        }else if(n%3==0){
+            return "Fizz";
+        }else if(n%5==0){
+            return "Buzz";
+        }else{
+            return "";
+        }
     }
 }
